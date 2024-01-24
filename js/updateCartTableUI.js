6cc04e50bcbd152becd8a1cd426073f051767ba1
Luis Martinez <mrluismartinezzz@gmail.com>
2024-01-23 16:32:27 -0800

    formats GrandTotal price

--- js/updateCartTableUI.js
+++ js/updateCartTableUI.js
@@ -1,15 +1,17 @@
 // Update Cart UI
 function updateCartTableUI() {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    if (cart.length) {
-        let grandTotal = 0;
-        const tableContent = cart.map((item) => {
-            grandTotal += (item.price * item.quantity)
-            return (`
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  if (cart.length) {
+    let grandTotal = 0;
+    const tableContent = cart.map((item) => {
+      grandTotal += item.price * item.quantity;
+      return `
                 <tr>
                     <td>
                         <div class="column_1">
-                            <img src="../images/${item.image}" alt="item image" />
+                            <img src="../images/${
+                              item.image
+                            }" alt="item image" />
                             <p>${item.name}</p>
                         </div>
                     </td>
@@ -21,15 +23,20 @@ function updateCartTableUI() {
 						</div>
                     </td>
                     <td>
-						$${item.price * item.quantity}
+						$${formatNum(item.price * item.quantity)}
 					</td>
                 </tr>
-            `)
-        })
-        $('#table').append(tableContent.join(""))
-        $('.summary_details').empty()
-        $('.summary_details').append(`<h2>Total</h2><h2>$${grandTotal}</h2>`)
-    } else {
-        //! handle nothing in your shopping cart ui
-    }
+            `;
+    });
+    $("#table").append(tableContent.join(""));
+    $(".summary_details").empty();
+    $(".summary_details").append(
+      `<h2>Total</h2><h2>$${formatNum(grandTotal)}</h2>`
+    );
+  } else {
+    //! handle nothing in your shopping cart ui
+  }
+}
+function formatNum(num) {
+  return parseFloat(num).toFixed(2);
 }
